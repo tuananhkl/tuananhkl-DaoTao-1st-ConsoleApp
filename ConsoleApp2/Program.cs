ea0ab74db8d7a80c39013b4f69c4506d88d9116a
Nguyen Tuan Anh <tuananhkl99@gmail.com>
2022-11-14 22:46:57 +0700

    Name I/O

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace ConsoleApp2 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Write("Nhập tên: ");
+            var name = Console.ReadLine();
+
+            Console.WriteLine($"Tên tôi là: {name}");
+        }
+    }
+}
