6e06e542b8995ab86b3506dcfbb75d961e59cdac
Nguyen Tuan Anh <tuananhkl99@gmail.com>
2022-11-14 22:49:36 +0700

    Age I/O added

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,8 +11,13 @@ namespace ConsoleApp2
 
             Console.Write("Nhập tên: ");
             var name = Console.ReadLine();
+            
+            Console.Write("Nhập tuổi: ");
+            var age = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine($"Tên tôi là: {name}");
+            Console.WriteLine($"Tôi năm nay {age} tuổi");
+
         }
     }
 }
